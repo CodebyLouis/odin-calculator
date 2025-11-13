06e0283566cca6aea0b771f5ea52440eac4789a7
Frozbee <louisrouyer65@gmail.com>
2025-11-13 08:05:39 +0100

    Implementation of the principal function to operate multiple ccalculation / New css style

--- script.js
+++ script.js
@@ -14,4 +14,63 @@ function operate (operator,a,b){
     else if(operator === "/"){return divide(a,b)}
 }
 
-console.log(operate ("+",3,5))
+const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator")
+const equalButton = document.querySelector("#equal");
+const display = document.querySelector(".display-operate");
+const displayHistory = document.querySelector(".display-history");
+const clear = document.querySelector("#clear");
+let currentNumber = "";
+
+function handleDigitClick() {
+    digitButtons.forEach(digit => {
+        digit.addEventListener("click", function() {
+            currentNumber = `${currentNumber}${digit.textContent}`;
+            display.textContent = currentNumber;
+            console.log(currentNumber)
+        });
+    });
+}
+
+function handleOperator (){
+    operatorButtons.forEach(ope => {
+        ope.addEventListener("click", function() {
+            if (operator ==="" && firstNumber ==""){
+                operator = ope.textContent
+                firstNumber = currentNumber
+                currentNumber = ""
+            }
+            else if (operator === "" && firstNumber !== "") {
+            operator = ope.textContent
+            currentNumber = ""
+            } 
+            else if(operator !==""){
+                handleCalculation()
+                operator = ope.textContent
+                currentNumber = ""
+            }
+        })
+    })
+}
+
+function handleEqual (){
+    equalButton.addEventListener("click", function () {
+        handleCalculation()
+        operator = "";
+        currentNumber = ""
+    })
+}
+
+function handleCalculation () {
+        secondNumber = currentNumber
+        let result = operate(operator, Number(firstNumber), Number(secondNumber))
+        display.textContent = result
+        displayHistory.textContent = `${firstNumber} ${operator} ${secondNumber}`
+        firstNumber = result
+}
+handleDigitClick();
+handleOperator()
+handleEqual()
+
+
+
