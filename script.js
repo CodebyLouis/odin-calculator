e95724ed70fae07796875adf3310035a38989ea2
Frozbee <louisrouyer65@gmail.com>
2025-11-14 08:28:50 +0100

    Add rounding function to limit decimal places in display

--- script.js
+++ script.js
@@ -21,13 +21,18 @@ const display = document.querySelector(".display-operate");
 const displayHistory = document.querySelector(".display-history");
 const clearAll = document.querySelector("#clear-all");
 const clearOne = document.querySelector("#clear-one");
+const colorChange = document.querySelector("#color-change")
 let currentNumber = "";
+
+function roundIfNeeded (num){
+    return Math.round(Number(num) * 1000) / 1000
+}
 
 function handleDigitClick() {
     digitButtons.forEach(digit => {
         digit.addEventListener("click", function() {
             currentNumber = `${currentNumber}${digit.textContent}`;
-            display.textContent = currentNumber;
+            display.textContent = roundIfNeeded(currentNumber);
             console.log(currentNumber)
         });
     });
@@ -37,7 +42,7 @@ function handleOperator (){
     operatorButtons.forEach(ope => {
         ope.addEventListener("click", function() {
             if (operator ==="" && firstNumber ==""){
-                operator = ope.textContent
+                operator = ope.id
                 firstNumber = currentNumber
                 currentNumber = ""
             }
@@ -45,12 +50,12 @@ function handleOperator (){
                 if (currentNumber !== "") {
                     firstNumber = currentNumber  
                 }
-                operator = ope.textContent
+                operator = ope.id
                 currentNumber = ""
             }
             else if(operator !==""){
                 handleCalculation()
-                operator = ope.textContent
+                operator = ope.id
                 currentNumber = ""
             }
         })
@@ -59,17 +64,22 @@ function handleOperator (){
 
 function handleEqual (){
     equalButton.addEventListener("click", function () {
+        if (operator ===""){
+            console.log("NA")
+        }
+        else {
         handleCalculation()
         operator = "";
         currentNumber = ""
+        }
     })
 }
 
 function handleCalculation () {
         secondNumber = currentNumber
         let result = operate(operator, Number(firstNumber), Number(secondNumber))
-        display.textContent = result
-        displayHistory.textContent = `${firstNumber} ${operator} ${secondNumber}`
+        display.textContent = roundIfNeeded(result)
+        displayHistory.textContent = `${roundIfNeeded(firstNumber)} ${operator} ${roundIfNeeded(secondNumber)}`
         firstNumber = result
 }
 handleDigitClick();
@@ -81,7 +91,7 @@ clearAll.addEventListener ("click", function(){
     firstNumber="";
     operator = "";
     secondNumber = "";
-    displayHistory.textContent = "CodeByLouis"
+    displayHistory.textContent = "Hello user !"
     display.textContent = "0";
     console.log("test")
 });
@@ -96,3 +106,12 @@ clearOne.addEventListener("click", function () {
     display.textContent = currentNumber;
 
 })
+
+colorChange.addEventListener("click",function(){
+    let hexa = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    document.documentElement.style.setProperty('--primary-color', hexa);
+})
+
+document.querySelector("#github-button").addEventListener("click", function() {
+    window.open("https://github.com/CodeByLouis", "_blank");
+});
